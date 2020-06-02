0e00c3b051690f63f81afd962e8419e0a909694a
god-data-unimib <a.pontini1@campus.unimib.it>
2020-06-02 16:32:13 +0200

    sistemato tutti i NAN

--- Lavoro Ale/Polonia_mercati.R
+++ Lavoro Ale/Polonia_mercati.R
@@ -52,20 +52,226 @@ df_consumi$DateYYYYMMDD <- NULL
 
 totale <- left_join(df_prezzi, df_consumi, by = c("anno" = "anno", "mese" = "mese", "giorno"="giorno", "ora"="ora"))
 totale <- totale[, -c(9:10)]
+
 ##########################################
-############## nan #######################
+########### NAN prezzi ###################
 ##########################################
 
-summary(df_prezzi)
-new_DF<-subset(df_prezzi,is.na(df_prezzi$prezzo))
+summary(totale)
+new_DF<-subset(totale,is.na(totale$prezzo))
 
 # dobbiamo sistemare na.
 
 # 14/03/2013 è un giovedì e va sistemato. provo a vedere il giorno successivo e precedente
-df_prezzi %>% filter(anno=="2013",
+totale %>% filter(anno=="2013",
                      mese=="03",
-                     giorno==c("13","15"))
-str(df_prezzi)
+                     giorno==c("15")) %>% 
+          select(anno,mese,giorno,ora,prezzo) -> fixna15
+
+totale %>% filter(anno=="2013",
+                  mese=="03",
+                  giorno==c("13")) %>% 
+  select(anno,mese,giorno,ora,prezzo) -> fixna13
+
+fixna15 <- left_join(fixna15, fixna13, by = c("anno" = "anno", "mese" = "mese","ora"="ora"))
+
+fixna15$giorno.y <- NULL
+fixna15$giorno.x <- NULL
+names(fixna15)[4] <- "prezzo15"
+names(fixna15)[5] <- "prezzo13"
+
+fixna15$prezzi14 <- apply(fixna15[,c(4,5)], 1, mean)
+
+fixna15$giorno14 <- "14"
+
+totale <- left_join(totale, fixna15, by = c("anno" = "anno", "mese" = "mese", "giorno"="giorno14", "ora"="ora"))
+str(totale)
+?if_else
+totale %>% mutate(prezzo = if_else((giorno =="14") & (anno == "2013") & (mese == "03"),prezzi14,prezzo)) -> totale
+new_DF<-subset(totale,is.na(totale$prezzo))
+
+# 21/03/2013 provo a vedere il giorno successivo e precedente
+totale %>% filter(anno=="2013",
+                  mese=="03",
+                  giorno==c("20")) %>% 
+  select(anno,mese,giorno,ora,prezzo) -> fixna15
+
+totale %>% filter(anno=="2013",
+                  mese=="03",
+                  giorno==c("22")) %>% 
+  select(anno,mese,giorno,ora,prezzo) -> fixna13
+
+fixna15 <- left_join(fixna15, fixna13, by = c("anno" = "anno", "mese" = "mese","ora"="ora"))
+
+fixna15$giorno.y <- NULL
+fixna15$giorno.x <- NULL
+names(fixna15)[4] <- "prezzo20"
+names(fixna15)[5] <- "prezzo22"
+
+fixna15$prezzi21 <- apply(fixna15[,c(4,5)], 1, mean)
+
+fixna15$giorno21 <- "21"
+
+totale <- left_join(totale, fixna15, by = c("anno" = "anno", "mese" = "mese", "giorno"="giorno21", "ora"="ora"))
+
+totale %>% mutate(prezzo = if_else((giorno =="21") & (anno == "2013") & (mese == "03"),prezzi21,prezzo)) -> totale
+
+totale <- totale[, -c(9:15)]
+
+new_DF <- subset(totale,is.na(totale$prezzo))
+
+# 12/06/2013 mancano le 24
+totale %>% filter(anno=="2013",
+                  mese=="06",
+                  giorno==c("11"),
+                  ora=="24:00:00") %>% 
+  select(anno,mese,giorno,ora,prezzo) -> fixna15
+
+totale %>% filter(anno=="2013",
+                  mese=="06",
+                  giorno==c("14"),
+                  ora=="24:00:00") %>% 
+  select(anno,mese,giorno,ora,prezzo) -> fixna13
+
+fixna15 <- left_join(fixna15, fixna13, by = c("anno" = "anno", "mese" = "mese","ora"="ora"))
+
+fixna15$giorno.y <- NULL
+fixna15$giorno.x <- NULL
+names(fixna15)[4] <- "prezzo11"
+names(fixna15)[5] <- "prezzo14"
+
+fixna15$prezzi12 <- apply(fixna15[,c(4,5)], 1, mean)
+
+fixna15$giorno12 <- "12"
+fixna15
+totale <- left_join(totale, fixna15, by = c("anno" = "anno", "mese" = "mese", "giorno"="giorno12", "ora"="ora"))
+
+totale %>% mutate(prezzo = if_else((giorno =="12") & (anno == "2013") & (mese == "06") & (ora=="24:00:00"), prezzi12, prezzo)) -> totale
+#totale <- totale[, -c(9:15)]
+
+new_DF <- subset(totale,is.na(totale$prezzo))
+
+# 13/06/2013 tutta la giornata
+totale %>% filter(anno=="2013",
+                  mese=="06",
+                  giorno==c("12")) %>% 
+  select(anno,mese,giorno,ora,prezzo) -> fixna15
+
+totale %>% filter(anno=="2013",
+                  mese=="06",
+                  giorno==c("14")) %>% 
+  select(anno,mese,giorno,ora,prezzo) -> fixna13
+
+fixna15 <- left_join(fixna15, fixna13, by = c("anno" = "anno", "mese" = "mese","ora"="ora"))
+
+fixna15$giorno.y <- NULL
+fixna15$giorno.x <- NULL
+names(fixna15)[4] <- "prezzo12"
+names(fixna15)[5] <- "prezzo14"
+
+fixna15$prezzi13 <- apply(fixna15[,c(4,5)], 1, mean)
+
+fixna15$giorno13 <- "13"
+fixna15
+totale <- left_join(totale, fixna15, by = c("anno" = "anno", "mese" = "mese", "giorno"="giorno13", "ora"="ora"))
+
+totale %>% mutate(prezzo = if_else((giorno =="13") & (anno == "2013") & (mese == "06"), prezzi13, prezzo)) -> totale
+totale <- totale[, -c(9:15)]
+
+new_DF <- subset(totale,is.na(totale$prezzo))
+
+# 23/01/2014 tutta la giornata vedere perchè siamo in festività
+totale %>% filter(anno=="2014",
+                  mese=="01",
+                  giorno==c("22")) %>% 
+  select(anno,mese,giorno,ora,prezzo) -> fixna15
+
+totale %>% filter(anno=="2014",
+                  mese=="01",
+                  giorno==c("24")) %>% 
+  select(anno,mese,giorno,ora,prezzo) -> fixna13
+
+fixna15 <- left_join(fixna15, fixna13, by = c("anno" = "anno", "mese" = "mese","ora"="ora"))
+
+fixna15$giorno.y <- NULL
+fixna15$giorno.x <- NULL
+names(fixna15)[4] <- "prezzo22"
+names(fixna15)[5] <- "prezzo24"
+
+fixna15$prezzi23 <- apply(fixna15[,c(4,5)], 1, mean)
+
+fixna15$giorno23 <- "23"
+fixna15
+totale <- left_join(totale, fixna15, by = c("anno" = "anno", "mese" = "mese", "giorno"="giorno23", "ora"="ora"))
+
+totale %>% mutate(prezzo = if_else((giorno =="23") & (anno == "2014") & (mese == "01"), prezzi23, prezzo)) -> totale
+#totale <- totale[, -c(9:15)]
+
+new_DF <- subset(totale,is.na(totale$prezzo))
+
+# 15/07/2014 mancano le 24
+totale %>% filter(anno=="2014",
+                  mese=="07",
+                  giorno==c("14"),
+                  ora=="24:00:00") %>% 
+  select(anno,mese,giorno,ora,prezzo) -> fixna15
+
+totale %>% filter(anno=="2014",
+                  mese=="07",
+                  giorno==c("17"),
+                  ora=="24:00:00") %>% 
+  select(anno,mese,giorno,ora,prezzo) -> fixna13
+
+fixna15 <- left_join(fixna15, fixna13, by = c("anno" = "anno", "mese" = "mese","ora"="ora"))
+
+fixna15$giorno.y <- NULL
+fixna15$giorno.x <- NULL
+names(fixna15)[4] <- "prezzo14"
+names(fixna15)[5] <- "prezzo17"
+
+fixna15$prezzi15 <- apply(fixna15[,c(4,5)], 1, mean)
+
+fixna15$giorno15 <- "15"
+fixna15
+totale <- left_join(totale, fixna15, by = c("anno" = "anno", "mese" = "mese", "giorno"="giorno15", "ora"="ora"))
+
+totale %>% mutate(prezzo = if_else((giorno =="15") & (anno == "2014") & (mese == "07") & (ora=="24:00:00"), prezzi15, prezzo)) -> totale
+totale <- totale[, -c(9:15)]
+
+new_DF <- subset(totale,is.na(totale$prezzo))
+
+# 16/07/2014 manca la giornata intera
+totale %>% filter(anno=="2014",
+                  mese=="07",
+                  giorno==c("15")) %>% 
+  select(anno,mese,giorno,ora,prezzo) -> fixna15
+
+totale %>% filter(anno=="2014",
+                  mese=="07",
+                  giorno==c("17")) %>% 
+  select(anno,mese,giorno,ora,prezzo) -> fixna13
+
+fixna15 <- left_join(fixna15, fixna13, by = c("anno" = "anno", "mese" = "mese","ora"="ora"))
+
+fixna15$giorno.y <- NULL
+fixna15$giorno.x <- NULL
+names(fixna15)[4] <- "prezzo15"
+names(fixna15)[5] <- "prezzo17"
+
+fixna15$prezzi16 <- apply(fixna15[,c(4,5)], 1, mean)
+
+fixna15$giorno16 <- "16"
+fixna15
+totale <- left_join(totale, fixna15, by = c("anno" = "anno", "mese" = "mese", "giorno"="giorno16", "ora"="ora"))
+
+totale %>% mutate(prezzo = if_else((giorno =="16") & (anno == "2014") & (mese == "07"), prezzi16, prezzo)) -> totale
+
+new_DF <- subset(totale,is.na(totale$prezzo))
+totale <- totale[, -c(9:11)]
+
+#Sistemato tutti i nan
+summary(totale$prezzo)
+
 # CREO DATASET PER OGNI ANNO
 lista_anni <- c(2012,2013,2014,2015)
 datasets <- list()
