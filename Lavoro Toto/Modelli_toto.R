73056525b84d83d789796d84698b729c803e3d94
vizzi5 <svvizzisv@gmail.com>
2020-07-06 20:09:17 +0200

    dev_off_scandi

--- Lavoro Toto/Modelli_toto.R
+++ Lavoro Toto/Modelli_toto.R
@@ -89,7 +89,7 @@ fit10
 ds_ita8completo <- read.csv("C:\\Users\\vizzi\\PROG_DSLAB_GITHUB\\Progetto_DSLAB\\DATASET SERIE STORICHE\\italia8mattina.csv")
 
 ds_ita8
-ds_ita8<-  ts(coredata(ds_ita8[,"TSTOT.ConsumiITA"]), freq = 365, start = c(2012,1,1),  end = c(2015,365))
+ds_ita8<-  ts(coredata(ds_ita8completo[,"TSTOT.ConsumiITA"]), freq = 365, start = c(2012,1,1),  end = c(2015,365))
 
 x7 <- fourier(ds_ita8, K=16)
 
@@ -107,13 +107,43 @@ fit9 %>%  forecast(xreg=x7, h=1 )%>%
 ########################################################
 
 fit9 %>%
-  forecast(xreg=fourier(ds_ita8, K=16, h=7)) %>%
-  autoplot(include=31)
+  forecast(xreg=fourier(ds_ita8, K=16, h=365)) %>%
+  autoplot(include=365)
 
 fit9 %>%
   forecast(xreg=fourier(ds_ita8, K=16, h=7)) ->fit10
 
 fit10
+
+
+
+
+######################################
+
+
+ds_ita8completo <- read.csv("C:\\Users\\vizzi\\PROG_DSLAB_GITHUB\\Progetto_DSLAB\\DATASET SERIE STORICHE\\italia8mattina.csv")
+
+ds_ita8
+ds_ita8<-  ts(coredata(ds_ita8completo[,"TSTOT.ConsumiITA"]), freq = 365, start = c(2012,1,1),  end = c(2015,365))
+
+# x7 <- fourier(ds_ita8, K=16)
+
+# xf<-ds8[,"TSTOT.ConsumiITA"]
+
+fit9 <- auto.arima(ds_ita8, seasonal=T, lambda=0, trace = T)
+
+autoplot(fit9)
+
+summary(fit9)
+
+
+fit9 %>%  forecast(xreg=x7, h=1 )%>%
+  autoplot()
+########################################################
+
+fit9 %>%
+  forecast(h=365) %>%
+  autoplot(include=365)
 ########################################################################
 ############################## Dataset Scandinavia
 ds_sca8 <- read.csv("C:\\Users\\vizzi\\PROG_DSLAB_GITHUB\\Progetto_DSLAB\\DATASET SERIE STORICHE\\scandinavia8mattina.csv")
@@ -159,7 +189,34 @@ fitsca %>%
   forecast(xreg=fourier(ds_sca8, K=4, h=7)) %>%
   autoplot(include=31)
 
+#fitsca %>%
+ #  forecast(xreg=fourier(ds_sca8, K=16, h=7)) ->fit10
+
+##################################################################
+
+ds_sca8 <- read.csv("C:\\Users\\vizzi\\PROG_DSLAB_GITHUB\\Progetto_DSLAB\\DATASET SERIE STORICHE\\scandinavia8mattina.csv")
+
+ds_sca8
+ds_sca8<-  ts(coredata(ds_sca8[,"TSTOT.ConsumiScandi"]), freq = 365, start = c(2012,1,1),  end = c(2015,365))
+
+xsca <- fourier(ds_sca8, K=4)
+
+fitsca <- auto.arima(ds_sca8, seasonal=T, lambda=0, xreg=xsca, trace = T)
+
+autoplot(fitsca)
+
+summary(fitsca)
+
+library(ggplot2)
 fitsca %>%
-  forecast(xreg=fourier(ds_sca8, K=16, h=7)) ->fit10
+  forecast(xreg=fourier(ds_sca8, K=4, h=7)) %>%
+  autoplot(include=31) + xlab("Anni") + ylab("Consumi in KW") +
+  ggtitle("Previsione consumi Scandinavia con ARIMA(5,1,2)")
 
+png("plotscandinaviarima.png",4032,3024, units="px", res=500)
+fitsca %>%
+  forecast(xreg=fourier(ds_sca8, K=4, h=7)) %>%
+  autoplot(include=31) + xlab("Anni") + ylab("Consumi in KW") +
+  ggtitle("Previsione consumi Scandinavia con ARIMA(5,1,2)")
 
+dev.off()
